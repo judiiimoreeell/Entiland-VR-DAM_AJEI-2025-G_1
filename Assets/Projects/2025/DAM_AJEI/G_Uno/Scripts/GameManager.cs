69f7d370a4f59d270ecd07a7375349542f61dbde
Vanderjon <146344183+Vanderjon@users.noreply.github.com>
2025-02-19 17:11:58 +0100

    GameManager Creado

---
+++ Assets/Projects/2025/DAM_AJEI/G_Uno/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EntilandVR.DosCinco.DAM_AJEI.G_Uno
+{
+
+
+    public class GameManager : MonoBehaviour
+    {
+    
+        public static GameManager Instance {  get; private set; }
+        // Start is called before the first frame update
+
+        public float puntuación;
+        private void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
