0dd256f1244a9c4ed779a804e7ec8d06e85720c2
Vanderjon <146344183+Vanderjon@users.noreply.github.com>
2025-02-26 19:05:57 +0100

    spline terminada

---
+++ Assets/Projects/2025/DAM_AJEI/G_Uno/Scripts/Bagoneta_spline.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+public class Bagoneta_spline : MonoBehaviour
+{
+    float contador;
+    public SplineAnimate spline;
+    public SplineContainer newContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spline = GetComponent<SplineAnimate>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(contador <= 5)
+        {
+            contador += Time.deltaTime;
+        }
+        else
+        {
+            spline.Container = newContainer;
+
+            spline.NormalizedTime = 0f;
+            spline.Play();
+
+            contador = 0;
+        }
+
+    }
+}
